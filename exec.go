978a71553afcb2b5412c728d82c37069d8073473
Chyroc <chyroc@qq.com>
2018-07-31 22:17:20 +0800

    init and query row

---
+++ exec.go
@@ -0,0 +1,28 @@
+package sqlfly
+
+import (
+	"database/sql"
+	"context"
+
+	_ "github.com/go-sql-driver/mysql" // 注册mysql driver
+)
+
+type queryRow struct {
+	row *sql.Row
+}
+
+func (r queryRow) Scan(dest ...interface{}) error {
+	return r.row.Scan(dest...)
+}
+
+func QueryRowContext(ctx context.Context, dber sql.DB, query string, args ...interface{}) queryRow {
+	return queryRow{row: dber.QueryRowContext(ctx, query, args...)}
+}
+
+func QueryRow(dber sql.DB, query string, args ...interface{}) queryRow {
+	return QueryRowContext(context.Background(), dber, query, args...)
+}
+
+func QueryContext() {
+
+}
