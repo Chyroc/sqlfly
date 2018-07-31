ad1b9bb28e78b41bc312f2853bf001dc2f5cb515
Chyroc <chyroc@qq.com>
2018-07-31 22:30:29 +0800

    query each

--- exec.go
+++ exec.go
@@ -1,8 +1,8 @@
 package sqlfly
 
 import (
-	"database/sql"
 	"context"
+	"database/sql"
 
 	_ "github.com/go-sql-driver/mysql" // 注册mysql driver
 )
@@ -15,14 +15,44 @@ func (r queryRow) Scan(dest ...interface{}) error {
 	return r.row.Scan(dest...)
 }
 
-func QueryRowContext(ctx context.Context, dber sql.DB, query string, args ...interface{}) queryRow {
+func QueryRowContext(ctx context.Context, dber *sql.DB, query string, args ...interface{}) queryRow {
 	return queryRow{row: dber.QueryRowContext(ctx, query, args...)}
 }
 
-func QueryRow(dber sql.DB, query string, args ...interface{}) queryRow {
+func QueryRow(dber *sql.DB, query string, args ...interface{}) queryRow {
 	return QueryRowContext(context.Background(), dber, query, args...)
 }
 
-func QueryContext() {
+type Generator func() (func(), []interface{})
 
+type queryRows struct {
+	*sql.Rows
+	err error
+}
+
+func (r queryRows) Each(g Generator) error {
+	if r.err != nil {
+		return r.err
+	}
+	defer r.Rows.Close()
+
+	for r.Rows.Next() {
+		f, dest := g()
+		if err := r.Rows.Scan(dest...); err != nil {
+			return err
+		}
+
+		f() // append
+	}
+
+	return r.Rows.Err()
+}
+
+func QueryContext(ctx context.Context, dber *sql.DB, query string, args ...interface{}) queryRows {
+	rows, err := dber.QueryContext(ctx, query, args...)
+	return queryRows{Rows: rows, err: err}
+}
+
+func Query(dber *sql.DB, query string, args ...interface{}) queryRows {
+	return QueryContext(context.Background(), dber, query, args)
 }
--- testdata/query_test.go
+++ testdata/query_test.go
@@ -1,7 +1,31 @@
 package testdata
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/Chyroc/sqlfly"
+)
 
 func TestQueryRow(t *testing.T) {
+	t.SkipNow()
 
+	t.Run("", func(t *testing.T) {
+		var id string
+
+		if err := sqlfly.QueryRow(nil, "select id from t limit 1", nil).Scan(&id); err != nil {
+			t.Fail()
+		}
+	})
+
+	t.Run("", func(t *testing.T) {
+		var ids []string
+		err := sqlfly.Query(nil, "select id from t limit 1", nil).Each(func() (func(), []interface{}) {
+			var id string
+			return func() { ids = append(ids, id) }, []interface{}{&id}
+		})
+
+		if err != nil {
+			t.Fail()
+		}
+	})
 }
